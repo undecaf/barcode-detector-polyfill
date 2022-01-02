05025f78831036815ea2ad48e122c12d38981873
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2022-01-02 12:51:20 +0100

    Avoid warnings when building the example

--- example/esbuild.config.js
+++ example/esbuild.config.js
@@ -1,8 +1,9 @@
 import esbuild from 'esbuild'
 import { replace } from 'esbuild-plugin-replace'
-import { ZBAR_WASM_PKG_NAME, ZBAR_WASM_REPOSITORY, ZBAR_WASM_VERSION } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
+import { ZBAR_WASM_REPOSITORY } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
 
 // Uncomment this to load module @undecaf/zbar-wasm from an alternate repository, e.g. from https://unpkg.com/
+// import { ZBAR_WASM_PKG_NAME, ZBAR_WASM_VERSION } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
 // const ALTERNATE_ZBAR_WASM_REPOSITORY = `https://unpkg.com/${ZBAR_WASM_PKG_NAME}@${ZBAR_WASM_VERSION}`
 
 const zbarWasmRepository =
--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,9 +1,10 @@
 import replace from '@rollup/plugin-replace'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import { ZBAR_WASM_PKG_NAME, ZBAR_WASM_REPOSITORY, ZBAR_WASM_VERSION } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
+import { ZBAR_WASM_REPOSITORY } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
 
 // Uncomment this to load module @undecaf/zbar-wasm from an alternate repository, e.g. from https://unpkg.com/
+// import { ZBAR_WASM_PKG_NAME, ZBAR_WASM_VERSION } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
 // const ALTERNATE_ZBAR_WASM_REPOSITORY = `https://unpkg.com/${ZBAR_WASM_PKG_NAME}@${ZBAR_WASM_VERSION}`
 
 const zbarWasmRepository =
