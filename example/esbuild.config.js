6f1657ca2d12cb19a97e9dd4ed613def14bb00b7
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2023-03-01 13:05:27 +0100

    Bundle zbar.wasm in examples

--- example/esbuild.config.js
+++ example/esbuild.config.js
@@ -1,27 +1,30 @@
 import esbuild from 'esbuild'
+import { copy } from 'esbuild-plugin-copy'
 import { replace } from 'esbuild-plugin-replace'
 import { ZBAR_WASM_REPOSITORY } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
-
-// Uncomment this to load module @undecaf/zbar-wasm from an alternate repository, e.g. from https://unpkg.com/
-// import { ZBAR_WASM_PKG_NAME, ZBAR_WASM_VERSION } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
-// const ALTERNATE_ZBAR_WASM_REPOSITORY = `https://unpkg.com/${ZBAR_WASM_PKG_NAME}@${ZBAR_WASM_VERSION}`
-
-const zbarWasmRepository =
-    (typeof ALTERNATE_ZBAR_WASM_REPOSITORY !== 'undefined') ? ALTERNATE_ZBAR_WASM_REPOSITORY : ZBAR_WASM_REPOSITORY
 
 const options = {
     entryPoints: ['./src/main.js'],
     bundle: true,
     outfile: '../docs/example/js/main.js',
     format: 'esm',
-    target: 'es2015',
+    target: 'es2020',
     minify: true,
     sourcemap: false,
     plugins: [
         replace({
             values: {
-                // Replaces the repository URL with the alternate repository URL if necessary
-                [ZBAR_WASM_REPOSITORY]: zbarWasmRepository,
+                // Replaces the repository URL with a local reference
+                [ZBAR_WASM_REPOSITORY]: '@undecaf/zbar-wasm',
+                '/dist/main.js': '',
+                '/dist/index.js': '',
+            },
+        }),
+
+        copy({
+            assets: {
+                from: ['node_modules/@undecaf/zbar-wasm/dist/zbar.wasm'],
+                to: ['.'],
             },
         }),
     ],
--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,35 +1,36 @@
+import copy from 'rollup-plugin-copy'
 import replace from '@rollup/plugin-replace'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import { ZBAR_WASM_REPOSITORY } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
 
-// Uncomment this to load module @undecaf/zbar-wasm from an alternate repository, e.g. from https://unpkg.com/
-// import { ZBAR_WASM_PKG_NAME, ZBAR_WASM_VERSION } from '@undecaf/barcode-detector-polyfill/zbar-wasm'
-// const ALTERNATE_ZBAR_WASM_REPOSITORY = `https://unpkg.com/${ZBAR_WASM_PKG_NAME}@${ZBAR_WASM_VERSION}`
-
-const zbarWasmRepository =
-    (typeof ALTERNATE_ZBAR_WASM_REPOSITORY !== 'undefined') ? ALTERNATE_ZBAR_WASM_REPOSITORY : ZBAR_WASM_REPOSITORY
-
 export default [
     {
         input: 'src/main.js',
         output: {
-            file: '../docs/example/js/main.js',
+            dir: '../docs/example/js/',
             format: 'esm',
             generatedCode: 'es2015',
             sourcemap: false,
         },
-        external: [
-            `${zbarWasmRepository}/dist/main.js`
-        ],
         plugins: [
             nodeResolve(),
             replace({
                 values: {
-                    // Replaces the repository URL with the alternate repository URL if necessary
-                    [ZBAR_WASM_REPOSITORY]: zbarWasmRepository,
+                    // Replaces the repository URL with a local reference
+                    [ZBAR_WASM_REPOSITORY]: '@undecaf/zbar-wasm',
+                    '/dist/main.js': '',
+                    '/dist/index.js': '',
                 },
                 preventAssignment: true,
+            }),
+            copy({
+                targets: [
+                    {
+                        src: 'node_modules/@undecaf/zbar-wasm/dist/zbar.wasm',
+                        dest: '../docs/example/js/'
+                    }
+                ],
             }),
             terser(),
         ],
